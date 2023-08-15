f3e8848b070a63647a33b3e267c6cac7f2b19820
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2023-08-15 12:56:24 -0400

    PitchCount Play PlayerPage 1
    I got the code behind to display the data.

--- Play/PitchCounter/PitchCounter/Views/Player.xaml.cs
+++ Play/PitchCounter/PitchCounter/Views/Player.xaml.cs
@@ -1,9 +1,36 @@
 namespace PitchCounter.Views;
+using PitchCounter.Data;
 
 public partial class Player : ContentPage
 {
-	public Player()
+	public Models.Player aPlayer;
+    public DataBase data = new DataBase();
+	//defines the array, yes it is repetitive and there are better ways to do this MVVM
+	public int playerIndex = 0;
+	Random rnd = new Random();
+    public Player()
 	{
 		InitializeComponent();
+		showPlayer();
 	}
+
+	public void showPlayer()
+	{
+        playerIndex = (int)rnd.Next(4);
+        aPlayer = data.PlayerRay[playerIndex];
+		BallLabel.Text = aPlayer.Balls.ToString();
+        StrikeLabel.Text = aPlayer.Strikes.ToString();
+        TotalLabel.Text = aPlayer.Total.ToString();
+        NameLabel.Text = aPlayer.Name.ToString();
+    }
+
+	protected async override void OnAppearing()
+	{
+		base.OnAppearing();
+        playerIndex = (int)rnd.Next(4);
+		showPlayer();
+    }
+	
+	
+
 }
