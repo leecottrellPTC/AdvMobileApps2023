8a8b0f64506f6577b0e916f3de994204c31c9fa8
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2023-08-22 10:01:25 -0400

    PitchCount MVVM

--- Week 4-6 - Baseball/PitchCounter/PitchCounter/Models/PlayerClass.cs
+++ Week 4-6 - Baseball/PitchCounter/PitchCounter/Models/PlayerClass.cs
@@ -4,17 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.ComponentModel;
 
 namespace PitchCounter.Models
 {
-    public class PlayerClass
+    public class PlayerClass:INotifyPropertyChanged
     {
-		private int _balls;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+
+                PropertyChanged(this, args);
+            }
+        }
+
+        private int _balls;
 
 		public int Balls
 		{
 			get { return _balls; }
-			set { _balls = value; }
+			set { _balls = value;
+                Total = _balls + _strikes;
+                OnPropertyChanged("Balls");
+                }
 		}
 
 		private int _strikes;
@@ -22,7 +38,10 @@ namespace PitchCounter.Models
 		public int Strikes
 		{
 			get { return _strikes; }
-			set { _strikes = value; }
+			set { _strikes = value;
+                Total = _balls + _strikes;
+                OnPropertyChanged("Strikes");
+            }
 		}
 
 		private int _total;
@@ -30,12 +49,16 @@ namespace PitchCounter.Models
 		public int Total
 		{
 			get { return _total; }
-			set { _total = value; }
+			set { _total = value;
+                OnPropertyChanged("Total");
+            }
 		}
 
 		private string _name;
 
-		public string Name
+        
+
+        public string Name
 		{
 			get { return _name; }
 			set { _name = value; }
@@ -49,7 +72,14 @@ namespace PitchCounter.Models
             _name = name;
         }
 
-		public PlayerClass()
+        public PlayerClass(int balls, int strikes, int total)
+        {
+            _balls = balls;
+            _strikes = strikes;
+            _total = total;
+        }
+
+        public PlayerClass()
 		{
             _balls = 0;
             _strikes = 0;
---
+++ Week 4-6 - Baseball/PitchCounter/PitchCounter/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PitchCounter.Models;
+using System.ComponentModel;
+
+namespace PitchCounter.ViewModels
+{
+    public class PlayerViewModel : INotifyPropertyChanged
+    {
+        public Command AddBallCommand { get; set; }
+        public Command AddStrikeCommand { get; set; }
+        public Command ResetCommand { get; set; }
+
+        public void Reset()
+        {
+            _thePitcher.Balls = 0;
+            _thePitcher.Strikes = 0;
+            _thePitcher.Total = 0;
+        }
+
+        public PlayerViewModel()
+        {
+            //link the public Command to the logic function
+            AddBallCommand = new Command(AddBall);
+            AddStrikeCommand = new Command(AddStrike);
+            ResetCommand = new Command(Reset);
+            _thePitcher = new PlayerClass();
+        }
+        public void AddBall()
+        {
+            _thePitcher.Balls++;
+        }
+
+        public void AddStrike() 
+        {
+            _thePitcher.Strikes++;
+        }
+        private PlayerClass _thePitcher;
+
+        public PlayerClass ThePitcher
+        {
+            get { return _thePitcher; }
+            set { _thePitcher = value; }
+        }
+
+        //default Constructor
+   
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+               // PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+
+                PropertyChanged(this, args);
+            }
+        }
+    }
+}
