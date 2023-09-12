d4b7aadda32c96e6c688362be2696702eec7f61c
Lee Cottrell <cottrell.lee@ptcollege.edu>
2023-09-11 22:08:25 -0400

    Likert Plan Finished

---
+++ Play/CottrellLibrary2/CottrellLibrary2/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CottrellLibrary2
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ Play/CottrellLibrary2/CottrellLibrary2/Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace CottrellLibrary2
+{
+    // All the code in this file is only included on Windows.
+    public class PlatformClass1
+    {
+    }
+}
