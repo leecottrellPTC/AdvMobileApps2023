99593c6ed8e07d7c2ae80fe056ed402038dbd088
Lee Cottrell <cottrell.lee@ptcollege.edu>
2023-09-11 20:24:21 -0400

    likert

---
+++ Play/LikertPlan/LikertPlan/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace LikertPlan;
+
+public partial class MainPage : ContentPage
+{
+    public int Value { get; set; }
+    public void ChoiceChange(object sender, EventArgs e)
+	{
+		RadioButton rad = (RadioButton)sender;
+		if (rad.IsChecked == true)
+		{
+			Value = Convert.ToInt16(rad.Value.ToString());
+			DisplayAlert("Choice", Value.ToString(), "OK");
+		}
+    }
+
+
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
